873bd3d69a28e676a140a6951bea14eb7184c7a5
melovitor <vitormelobatista@hotmail.com>
2023-07-24 16:27:19 -0300

    WIP - ESLint.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/themes/global'
@@ -6,9 +5,9 @@ import { GlobalStyle } from './styles/themes/global'
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <GlobalStyle/>
       <h1>Timer</h1>
 
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
