ea921d69ab7d2ae55525c573dd03b4092ab26cfe
melovitor <vitormelobatista@hotmail.com>
2023-07-27 15:24:40 -0300

    WIP - Separando Action Types.

--- src/contexts/CycleContext.tsx
+++ src/contexts/CycleContext.tsx
@@ -1,13 +1,5 @@
 import { ReactNode, createContext, useState, useReducer } from 'react'
-
-interface Cycle {
-  id: string
-  task: string
-  minutesAmount: number
-  startDate: Date
-  interruptedDate?: Date
-  finishedDate?: Date
-}
+import { ActionTypes, Cycle, CyclesReducer } from '../reducers/cycles'
 
 interface CreateCycleData {
   task: string
@@ -32,58 +24,15 @@ interface CyclesContextProviderProps {
   children: ReactNode
 }
 
-interface CyclesState {
-  cycles: Cycle[]
-  activeCycleId: string | null
-}
-
 export function CyclesContextProvider({
   children,
 }: CyclesContextProviderProps) {
-  const [cyclesState, dispath] = useReducer(
-    (state: CyclesState, action: any) => {
-      switch (action.type) {
-        case 'ADD_NEW_CYCLE':
-          return {
-            ...state,
-            cycles: [...state.cycles, action.payload.newCycle],
-            activeCycleId: action.payload.newCycle.id,
-          }
-        case 'INTERRUPT_CURRENT_CYCLE':
-          return {
-            ...state,
-            cycles: state.cycles.map((cycle) => {
-              if (cycle.id === state.activeCycleId) {
-                return { ...cycle, interruptedDate: new Date() }
-              } else {
-                return cycle
-              }
-            }),
-            activeCycleId: null,
-          }
-        case 'MARK_CURRENT_CYCLE_AS_FINISHED':
-          return {
-            ...state,
-            cycles: state.cycles.map((cycle) => {
-              if (cycle.id === state.activeCycleId) {
-                return { ...cycle, finishedDate: new Date() }
-              } else {
-                return cycle
-              }
-            }),
-            activeCycleId: null,
-          }
-        default:
-          return state
-      }
-    },
-    {
-      cycles: [
-        /* Colocar o valor inicial do localStorage aqui */
-      ],
-      activeCycleId: null,
-    },
-  )
+  const [cyclesState, dispath] = useReducer(CyclesReducer, {
+    cycles: [
+      /* Colocar o valor inicial do localStorage aqui */
+    ],
+    activeCycleId: null,
+  })
 
   const { activeCycleId, cycles } = cyclesState
   const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
@@ -92,11 +41,12 @@ export function CyclesContextProvider({
 
   function markCurrentCycleAsFinished() {
     dispath({
-      type: 'MARK_CURRENT_CYCLE_AS_FINISHED',
+      type: ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED,
       payload: {
         activeCycleId,
       },
     })
+    setAmountSecondsPassed(0)
   }
 
   function setSecondsPassed(seconds: number) {
@@ -112,7 +62,7 @@ export function CyclesContextProvider({
     }
 
     dispath({
-      type: 'ADD_NEW_CYCLE',
+      type: ActionTypes.ADD_NEW_CYCLE,
       payload: {
         newCycle,
       },
@@ -122,11 +72,12 @@ export function CyclesContextProvider({
   function interruptCurrentCycle() {
     document.title = `Timer`
     dispath({
-      type: 'INTERRUPT_CURRENT_CYCLE',
+      type: ActionTypes.INTERRUPT_CURRENT_CYCLE,
       payload: {
         activeCycleId,
       },
     })
+    setAmountSecondsPassed(0)
   }
 
   return (
