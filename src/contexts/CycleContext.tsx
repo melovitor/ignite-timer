bdf76dc9b4ca2ea24119ad7017890f45543e0353
melovitor <vitormelobatista@hotmail.com>
2023-07-27 15:03:46 -0300

    WIP - Salvando um objeto no reducer.

--- src/contexts/CycleContext.tsx
+++ src/contexts/CycleContext.tsx
@@ -32,23 +32,48 @@ interface CyclesContextProviderProps {
   children: ReactNode
 }
 
+interface CyclesState {
+  cycles: Cycle[]
+  activeCycleId: string | null
+}
+
 export function CyclesContextProvider({
   children,
 }: CyclesContextProviderProps) {
-  const [cycles, dispath] = useReducer(
-    (state: Cycle[], action: any) => {
+  const [cyclesState, dispath] = useReducer(
+    (state: CyclesState, action: any) => {
       if (action.type === 'ADD_NEW_CYCLE') {
-        return [...state, action.payload.newCycle]
+        return {
+          ...state,
+          cycles: [...state.cycles, action.payload.newCycle],
+          activeCycleId: action.payload.newCycle.id,
+        }
+      }
+      if (action.type === 'INTERRUPT_CURRENT_CYCLE') {
+        return {
+          ...state,
+          cycles: state.cycles.map((cycle) => {
+            if (cycle.id === state.activeCycleId) {
+              return { ...cycle, interruptedDate: new Date() }
+            } else {
+              return cycle
+            }
+          }),
+          activeCycleId: null,
+        }
       }
 
       return state
     },
-    [
-      /* Colocar o valor inicial do localStorage aqui */
-    ],
+    {
+      cycles: [
+        /* Colocar o valor inicial do localStorage aqui */
+      ],
+      activeCycleId: null,
+    },
   )
 
-  const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+  const { activeCycleId, cycles } = cyclesState
   const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
 
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
@@ -92,31 +117,16 @@ export function CyclesContextProvider({
         newCycle,
       },
     })
-    // setCycles((state) => [...state, newCycle])
-    setActiveCycleId(newCycle.id)
-    setAmountSecondsPassed(0)
   }
 
   function interruptCurrentCycle() {
     document.title = `Timer`
-
     dispath({
       type: 'INTERRUPT_CURRENT_CYCLE',
       payload: {
         activeCycleId,
       },
     })
-
-    // setCycles((state) =>
-    //   state.map((cycle) => {
-    //     if (cycle.id === activeCycleId) {
-    //       return { ...cycle, interruptedDate: new Date() }
-    //     } else {
-    //       return cycle
-    //     }
-    //   }),
-    // )
-    setActiveCycleId(null)
   }
 
   return (
